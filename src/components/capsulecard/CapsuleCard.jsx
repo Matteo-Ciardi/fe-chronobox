d2686f75c99bce4f789b7ce21a062ca5de784504
MatteoCiardi <ciardimatteo99@gmail.com>
2025-11-18 17:25:26 +0100

    modificate card in homepage

--- src/components/capsulecard/CapsuleCard.jsx
+++ src/components/capsulecard/CapsuleCard.jsx
@@ -9,8 +9,8 @@ export default function CapsuleCard() {
 				<div className="container-capsule">
 					<div className="container-image">
 						<img
-							src="./src/assets/img/capsula-amore-verde.png"
-							alt="capsule-classic"
+							src="src/assets/img/capsula_di_coppia.png"
+							alt="capsule-couple"
 						/>
 					</div>
 
@@ -34,15 +34,15 @@ export default function CapsuleCard() {
 				<div className="container-capsule">
 					<div className="container-image">
 						<img
-							src="./src/assets/img/capsule-mini.jpg"
+							src="src/assets/img/ritratto_di_un_anno.png"
 							alt="capsule-mini"
 						/>
 					</div>
 
 					<div className="container-capsule-body">
-						<h5 className="capsule-title">Capsula Amicizia</h5>
+						<h5 className="capsule-title">Capsula "Il mio anno"</h5>
 						<p className="capsule-description">
-							Capsula da regalare ad un amico
+							Conserva i momenti speciali del tuo anno 
 						</p>
 						<div className="capsule-footer">
 							<span className="capsule-price">&euro;30,00</span>
@@ -58,15 +58,15 @@ export default function CapsuleCard() {
 				<div className="container-capsule">
 					<div className="container-image">
 						<img
-							src="./src/assets/img/capsule-premium.jpg"
+							src="src/assets/img/futuro_me.png"
 							alt="capsule-premium"
 						/>
 					</div>
 
 					<div className="container-capsule-body">
-						<h5 className="capsule-title">Capsula Laurea</h5>
+						<h5 className="capsule-title">Capsula "Futuro me"</h5>
 						<p className="capsule-description">
-							Capsula da regalare per una laurea
+							Conserva buoni propositi per un futuro te
 						</p>
 
 						<div className="capsule-footer">
