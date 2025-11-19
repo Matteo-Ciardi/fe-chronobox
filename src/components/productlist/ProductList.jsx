058b18960cc42931f9641588cee69c8181142616
MatteoCiardi <ciardimatteo99@gmail.com>
2025-11-19 22:01:39 +0100

    aggiunta select per ordinamento

--- src/components/productlist/ProductList.jsx
+++ src/components/productlist/ProductList.jsx
@@ -4,8 +4,20 @@ const ProductList = () => {
     return (
         <>
             <div className='product-list-container'>
-                <input type='text' name='searchbar' placeholder='Cerca..'></input>
-                <select 
+                <section className='filters'>
+                    <input type='text' name='searchbar' placeholder='Cerca..'></input>
+                    <select name='order'>
+                        <option value="">Scegli</option>
+                        <option value="Prezzo Alto-Basso">Prezzo Alto-Basso</option>
+                        <option value="Prezzo Basso-Alto">Prezzo Basso-Alto</option>
+                        <option value="Nome A-Z">Nome A-Z</option>
+                        <option value="Nome Z-A">Nome Z-A</option>
+                        <option value="Recenti-Meno Recenti">Recenti-Meno Recenti</option>
+                        <option value="Meno Recenti-Recenti">Meno Recenti-Recenti</option>
+                    </select>
+                </section>
+
+                
             </div>
         </>
     )
