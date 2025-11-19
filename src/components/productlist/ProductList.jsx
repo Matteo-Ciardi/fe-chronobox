e95583db875b028b1957e4a8b941144d1e1b4747
MatteoCiardi <ciardimatteo99@gmail.com>
2025-11-19 22:58:54 +0100

    creata logica per ricerca in pagina prodotti

--- src/components/productlist/ProductList.jsx
+++ src/components/productlist/ProductList.jsx
@@ -4,14 +4,20 @@ import { useProducts } from "../../context/DefaultContext";
 
 import './ProductList.css'
 
-const ProductList = () => {
-
+const ProductList = ({ searchTerm = '' }) => {
     const { products } = useProducts();
+
+    const normalizedSearch = searchTerm.toLowerCase()
+
+    const filteredProducts = products.filter((product) => {
+        const productName = product.name || product.title || ''
+        return productName.toLowerCase().includes(normalizedSearch) 
+    })
 
     return (
         <>
             <div className='product-list-container'>
-                {products.map((product) => (
+                {filteredProducts.map((product) => (
                     <CapsuleCard key={product.id} product={product} />
                 ))}
             </div>
