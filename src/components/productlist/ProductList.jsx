d4d9147b457875c59de89d5dd4de96a0a6e63873
MatteoCiardi <ciardimatteo99@gmail.com>
2025-11-19 21:55:52 +0100

    aggiunta searchbar

--- src/components/productlist/ProductList.jsx
+++ src/components/productlist/ProductList.jsx
@@ -1,12 +1,11 @@
-import CapsuleCard from '../../components/capsulecard/CapsuleCard'
-
 import './ProductList.css'
 
 const ProductList = () => {
     return (
         <>
             <div className='product-list-container'>
-                <CapsuleCard />
+                <input type='text' name='searchbar' placeholder='Cerca..'></input>
+                <select 
             </div>
         </>
     )
