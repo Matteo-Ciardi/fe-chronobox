523db7a1bb8751bc0c867b0ae10175c17493f006
fedstrial <fedstrial@proton.me>
2025-11-25 20:38:31 +0100

    fix typo

--- src/pages/detailpage/DetailPage.jsx
+++ src/pages/detailpage/DetailPage.jsx
@@ -333,7 +333,7 @@ export default function DetailPage() {
 												setShowModal(true);
 											}}
 										>
-											Aggiungi al Carrello
+											Personalizza
 										</button>
 									</div>
 								</div>
